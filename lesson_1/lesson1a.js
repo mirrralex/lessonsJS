1ecce212aa9914e9d25655de2fd49efd21e36f7c
Alex <mirrralex1@gmail.com>
2018-08-08 12:45:17 +0200

    Первый урок усложненное

---
+++ lesson_1/lesson1a.js
@@ -0,0 +1,11 @@
+var num = 33721;
+var stringNum = "";
+stringNum = num;
+var orderNumb = ('' + num).split('');
+var multip = 1;
+for (var i = 0; i <= 4; i++){
+	multip = multip * orderNumb[i];
+}
+multip = Math.pow(multip, 3);
+var twoNum = "" + multip;
+alert(twoNum[0] + "" + twoNum[1]);
