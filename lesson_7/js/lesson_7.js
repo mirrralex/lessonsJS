c5e03c2621f3aad184d86f52fb1f924773563f71
Alex <mirrralex1@gmail.com>
2018-08-20 20:55:28 +0200

    7 задание основное правки

--- lesson_7/js/lesson_7.js
+++ lesson_7/js/lesson_7.js
@@ -1,19 +1,34 @@
 /*jshint esversion: 6 */
-let dom = document.getElementsByTagName('div')[0];
-	date = new Date(2000, 0, 6, 8, 9, 7, 4, 3 ),
-	hour = date.getHours(),
-	min = date.getMinutes(),
-	sec = date.getSeconds();
-if(hour < 10) {
-	hour = "0" + hour;
-}
-if(min < 10) {
-	min = "0" + min;
-}
-if(sec < 10) {
-	sec = "0" + sec;
-}
+let dom = document.getElementsByTagName('div')[0],
+	startTime = new Date(),
+	lastTime = new Date();
+	lastTime.setMinutes(startTime.getMinutes() + 1);
 
-console.log(dom);
+while (startTime < lastTime){
 
-dom.textContent =  dom.textContent + hour + '.' + min + '.' + sec;
+	let	hour = startTime.getHours(),
+		min = startTime.getMinutes(),
+		sec = startTime.getSeconds();
+
+	if(hour < 10) {
+
+		hour = "0" + hour;
+
+	}
+
+	if(min < 10) {
+
+		min = "0" + min;
+
+	}
+
+	if(sec < 10) {
+
+		sec = "0" + sec;
+
+	}
+
+	dom.textContent = dom.textContent + '\n\r' + hour + '.' + min + '.' + sec;
+
+	startTime.setSeconds(startTime.getSeconds() + 1);
+}
