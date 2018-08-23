058cee883f2f99de081c774cbf7d1c1490206d59
Alex <mirrralex1@gmail.com>
2018-08-23 16:46:55 +0200

    10 урок поправил

--- lesson_10/JavaScript/lesson_10.js
+++ lesson_10/JavaScript/lesson_10.js
@@ -1,22 +1,23 @@
 /*jshint esversion: 6 */
-let options = class {
+class options {
 	constructor(height, width, backGround, fontSize, textAlign) {
 		this.height = height;
 	  this.width = width;
 	  this.backGround = backGround;
 	  this.fontSize = fontSize;
 	  this.textAlign = textAlign;
-  }
+	}
 	newDiv(name, text) {
 		let div = document.createElement('div');
 		div.className = '${name}';
-		div.textContent = '${text}';
+		div.innerHTML = '${text}';
 		div.height = this.height;
 		div.width = this.width;
 		div.backgroundColor = 'red';
 		div.fontSize = this.fontSize;
 		div.textAlign = this.textAlign;
+	return div;
 	}
 };
-let div = new options('10px', '10px', 'red', '100%', 'center');
-div.newDiv('first', 'abrakadabra');
+const div = new options('10px', '10px', 'green', '100%', 'center');
+console.log(div.newDiv('first', 'abrakadabra'));
