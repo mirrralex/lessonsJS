3872037e1d4e2e0f797124d26bcb89a2846fd90f
Alex <mirrralex1@gmail.com>
2018-08-23 17:32:19 +0200

    10 урок поправил

--- lesson_10/JavaScript/lesson_10.js
+++ lesson_10/JavaScript/lesson_10.js
@@ -7,17 +7,17 @@ class options {
 	  this.fontSize = fontSize;
 	  this.textAlign = textAlign;
 	}
-	newDiv(name, text) {
+	newDiv(name, text, place) {
 		let div = document.createElement('div');
 		div.className = '${name}';
-		div.innerHTML = '${text}';
+		div.innerHTML = text + ' ';
 		div.height = this.height;
 		div.width = this.width;
 		div.backgroundColor = 'red';
 		div.fontSize = this.fontSize;
 		div.textAlign = this.textAlign;
-	return div;
+		place.appendChild(div);
 	}
 };
 const div = new options('10px', '10px', 'green', '100%', 'center');
-console.log(div.newDiv('first', 'abrakadabra'));
+div.newDiv('first', 'abrakadabra', document.body);
