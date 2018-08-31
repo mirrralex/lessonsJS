b2b853a4ef532d012922e32254949677a5c8f59e
Alex <mirrralex1@gmail.com>
2018-08-31 17:35:26 +0200

    14 урок еще раз поправил

---
+++ lesson_15/JavaScript/script.js
@@ -0,0 +1,44 @@
+﻿// Функция sum должна возвращать тип данных true. Проверить её на это.
+
+function sum(a, b) {
+	return a + b > 10;
+}
+sum(2,2);
+// Переменная num должна быть равна 5. Проверить на соответствие.
+
+let arr = [ [1, 2, 3], [4, 5, 6], [7,8,9] ];
+let num = arr[1][1];
+// Узнать, что нам вернет функция each в данных условиях. Проверить её на тип данных, который она возвращает,
+// на соответствие ожидаемому результату (который вы получили) и на свойство length.
+
+var each = function(startArr, f){return f(startArr)};
+var aRR = [64, 49, 36, 25, 16];
+var myFunc = function(a){
+	var newArr = [];
+	for (var i = 0; i < a.length; i++) {
+		newArr[i]=Math.sqrt(a[i]);
+	}
+	return newArr;
+}
+console.log(each(aRR, myFunc));
+
+var assert = require('chai').assert;
+
+
+describe("sum", function() {
+	it("Sum возвращает true", function(){
+		assert.equals(sum, 'true');
+	})
+});
+console.log(num);
+describe("num", function() {
+	it("Num возвращает 5", function(){
+		assert.equals(num, 5);
+	})
+});
+
+describe("sum", function() {
+	it("Sum возвращает true", function(){
+		assert.equals(sum, 'true');
+	})
+});
